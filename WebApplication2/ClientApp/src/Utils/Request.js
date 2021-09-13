d01608182455eb1f4407593ad92750409376cbc3
theacidday <stanisyan16@gmail.com>
2021-09-13 12:31:04 +0300

    start using redux

--- WebApplication2/ClientApp/src/Utils/Request.js
+++ WebApplication2/ClientApp/src/Utils/Request.js
@@ -2,9 +2,9 @@
 const request = async (url, params = {}, body) => {
     const token = localStorage.getItem('token')
     const request = await fetch(`https://localhost:5001/api/${url}`, {
-        headers: { 'Content-type': 'application/json', 'Authorization': token },
-        /*...parmas,*/
-        /*...(body && { body: JSON.stringify(body) })*/
+        headers: { 'Content-type': 'application/json', ...(token && { 'Authorization': token }) },
+        ...parmas,
+        ...(body && { body: JSON.stringify(body) })
     });
     return await request.json();
 }
--- WebApplication2/ClientApp/src/components/Login/index.jsx
+++ WebApplication2/ClientApp/src/components/Login/index.jsx
@@ -1,61 +1,79 @@
-﻿import React, { useState, useCallback } from 'react';
+﻿import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import './Login.module.css';
+import { createStore } from "redux";
+import { useDispatch, useSelector } from "react-redux";
+import request from '../../Utils/Request';
 
+const defaultState =
+{
+    userName: "",
+    password: "",
+    id: "",
+    firstName: "",
+    lastName: "",
+    Role: "",
+}
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "get_userName":
+            return { ...state, userName: state.userName = action.userName }
+        case "get_password":
+            return { ...state, userName: state.password = action.password }
+        default:
+            return state
+    }
+}
+const store = createStore(reducer);
 
 
 const Login = () => {
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const userName = useSelector(state => state.userName);
+    const password = useSelector(state => state.password);
     const [redirect, setRedirect] = useState(false);
-
-    const handleChangeEmail = useCallback(e => {
-        setUserName(e.target.value);
-    }, [setUserName]);
-    const handleChangePassword = useCallback(e => {
-        setPassword(e.target.value);
-    }, [setPassword]);
+    const addUserName = (userName) => {
+        dispatch({ type: "get_userName", payload: userName })
+    }
+    const addPassword = (password) => {
+        dispatch({ type: "get_password", payload: password })
+    }
+    const url = "auth/login";
     const handleRedirect = () => {
         history.push('/Register/');
     }
     const history = useHistory();
     const submit = async (e) => {
-        e.preventDefault();
-        const loginResult = await fetch('https://localhost:5001/api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-            body: JSON.stringify({
-                userName,
-                password
-            })
-        });
-        const { token } = await loginResult.json()
+        e.preventDefault()
+        const result = await request(url, { method: 'Post' }, { userName, password });
+        const { token } = await result.json()
         localStorage.setItem('token', token)
-        setRedirect (true);
+        setRedirect(true);
     }
+
     if (redirect) {
         return <Redirect to="/" />
     }
 
     return (
-
-        <form onSubmit={submit}>
-            <h1>Login</h1>
-            <input placeholder="Username" value={userName} onChange={handleChangeEmail} type="username" name="User Name" required />
-            <input placeholder="Password" value={password} onChange={handleChangePassword} type="password" name="Password" required />
-            <button name="button" type="submit">Submit</button>
-            <div>________</div>
-            <div>______</div>
-            <div>____</div>
-            <div>__</div>
-            <h5>First time here?</h5>
-            <button onClick={handleRedirect} name="submit">
-                Create account
+        <Provider store={store}>
+            <form onSubmit={submit}>
+                <h1>Login</h1>
+                <input placeholder="Username" value={userName} onChange={() => addUserName} type="username" name="User Name" required />
+                <input placeholder="Password" value={password} onChange={() => addPassword} type="password" name="Password" required />
+                <button name="button" type="submit">Submit</button>
+                <div>________</div>
+                <div>______</div>
+                <div>____</div>
+                <div>__</div>
+                <h5>First time here?</h5>
+                <button onClick={handleRedirect} name="submit">
+                    Create account
                 </button>
-        </form>
-
+            </form>
+        </Provider>
     );
 }
 export default Login;
